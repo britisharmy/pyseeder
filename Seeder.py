a88d9cd9bd225909d2f041aa934f5a002d269814
Geoffrey <vectorofficeautomation@gmail.com>
2019-11-23 21:45:07 +0300

    Update Seeder.py

--- Seeder.py
+++ Seeder.py
@@ -1,5 +1,4 @@
 class Seeder:
- #default file param in every method
   def __init__(self, firstname, lastname,salutation,gender,coordinates,image,telephone_number,country,unix_timestamp,date_of_birth,numbers,decimals,address,email,django_password,paragraph,credit_card_number,cvv,day,month,year,username,random_string,postal_code,json_object,post_title,random_list_item,laravel_password,ion_auth_password:
 
     self.firstname = firstname
